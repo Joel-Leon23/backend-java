2494167cbd77ebe7557233d9277f9b7fdb5fc50d
Joel Leon <joeleoncs@outlook.com>
2023-09-17 21:17:42 -0600

    remove unused import

--- src/main/java/com/springboot/backend/app/computers/models/service/ComputerServiceImpl.java
+++ src/main/java/com/springboot/backend/app/computers/models/service/ComputerServiceImpl.java
@@ -3,7 +3,6 @@ package com.springboot.backend.app.computers.models.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 

061a59d749a24ca655efe90184dab223137411b9
Joel Leon <joeleoncs@outlook.com>
2023-09-17 21:49:55 -0600

    update import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO Computers(processor, graphics_card, ram_gb) VALUES ('Intel Core i5', 'NVIDIA GeForce GTX 1660', 16);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('AMD Ryzen 7', 'AMD Radeon RX 5700', 32);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('Intel Core i7', 'NVIDIA GeForce RTX 3080', 64);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('AMD Ryzen 5', 'NVIDIA GeForce GTX 1050 Ti', 8);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('Intel Core i9', 'NVIDIA GeForce RTX 3090', 128);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('AMD Ryzen 9', 'AMD Radeon RX 6800 XT', 64);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('Intel Core i3', 'NVIDIA GeForce GTX 1650', 16);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('AMD Ryzen 3', 'NVIDIA GeForce RTX 3060', 32);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('Intel Pentium', 'AMD Radeon RX 550', 4);
+INSERT INTO Computers (processor, graphics_card, ram_gb) VALUES ('AMD Athlon', 'NVIDIA GeForce GTX 1660 Super', 8);
